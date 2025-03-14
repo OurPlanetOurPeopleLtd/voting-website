14ff7169776630885c512d86d01eb30fcff0ce94
Chris Pepper <chrispepper1989@gmail.com>
2025-03-14 16:16:59 +0000

    PR comments

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,7 +1,6 @@
 ﻿const LoadingPage = () => {
     return <div className="spinner-border text-primary" role="status">
         <span className="visually-hidden">Loading...</span>
-       
     </div>;
 };
 export default LoadingPage;
