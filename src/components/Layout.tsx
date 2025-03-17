99c153d6bb2d18fa913d09fbb3f605d072e757e4
Chris Pepper <chrispepper1989@gmail.com>
2025-03-17 21:09:29 +0000

    removing registration page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -102,14 +102,14 @@ export const LayoutTs = ({children, locale} : ILayout) => {
                                 setExpanded(false)
                                 return {}
                             }}></DynamicNavList>
-                            <div>
+                            {/*<div>
                                 <Nav.Link onClick={() => {
                                     setExpanded(false)
                                     return {}
                                 }} as={NavLink} key={"registration"} to={ "registration"}>
                                     Register
                                 </Nav.Link>
-                            </div>
+                            </div>*/}
                             <div className="main-nav__language">
                                 <FlagSelect currentLocale={locale ?? defaultLanguage}></FlagSelect>
                             </div>
