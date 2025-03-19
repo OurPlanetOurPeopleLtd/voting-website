64f67104164ea6c7816d2fae29e6411ec7e0ae00
James Bateson <jim.bateson@outlook.com>
2025-03-19 16:14:14 +0000

    style: mobile select dropdowns

--- src/components/FlagSelect.tsx
+++ src/components/FlagSelect.tsx
@@ -20,16 +20,17 @@ const FlagSelect = ({currentLocale}: {currentLocale:string}) => {
     };
 
     return (
-        <>
+        <div className="select-wrap">
             <label className="visually-hidden" htmlFor="country_select">Select language</label>
-            <select onChange={handleCountryChange} id="country_select" value={selectedCountry.code} style={{width:"unset"}}>
+            <select onChange={handleCountryChange} id="country_select" value={selectedCountry.code}>
                 {supportedCountries.map(country => (
                     <option key={country.code} value={country.code}>
                         {country.flag}
                     </option>
                 ))}
             </select>
-        </>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="28" aria-hidden="true" fill="#000"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
+        </div>
     );
 };
 
--- src/components/VoteResults.tsx
+++ src/components/VoteResults.tsx
@@ -3,7 +3,6 @@ import {DataStore} from "@aws-amplify/datastore";
 import React, {useEffect, useState} from "react";
 import {getCountryList} from "../repositories/utils/country";
 import {Doughnut} from "react-chartjs-2";
-import "./VoteResults.scss";
 import {ArcElement, Chart as ChartJS, Tooltip} from 'chart.js';
 import {Col, Row} from "react-bootstrap";
 import {
@@ -12,9 +11,9 @@ import {
     getYesTranslation
 } from "../repositories/utils/extraTranslations";
 
+import "./VoteResults.scss";
 
 export const VoteResults = ({questionId, locale}: { questionId: string, locale?: string }) => {
-
     const [yesVotes, setYesVotes] = useState(0);
     const [noVotes, setNoVotes] = useState(0);
     const yesText = getYesTranslation(locale ?? "en");
@@ -26,7 +25,6 @@ export const VoteResults = ({questionId, locale}: { questionId: string, locale?:
     };
 
     useEffect(() => {
-
         async function fetchVotes(country: string | undefined) {
             const no = (await DataStore.query(Vote, (v) => v.and(v => [v.choice.eq(Choice.NO),  country ? v.country.eq(country) : v.country.notContains(null)]))).length;
             const yes = (await DataStore.query(Vote, (v) => v.and(v => [v.choice.eq(Choice.YES), country ? v.country.eq(country) : v.country.notContains(null)]))).length;
@@ -44,9 +42,7 @@ export const VoteResults = ({questionId, locale}: { questionId: string, locale?:
         }
     }, [selectedCountry]);
 
-
     ChartJS.register(ArcElement, Tooltip); // Register required elements
-
 
     const chartData = {
         datasets: [
@@ -62,45 +58,41 @@ export const VoteResults = ({questionId, locale}: { questionId: string, locale?:
     };
 
     const chartOptions = {}
-
-
     interface Country {
         [code: string]: string;
     }
 
     const countries: Country = getCountryList();
-
     const [isHovered, setIsHovered] = useState(false);
 
-
     return (<div className={"vote-results"}>
-
         <Row style={{paddingBottom: 20}}>
-
             <Col>
-                <select value={""} onChange={handleCountryChange}>
-
-
-                    {selectedCountry ?
-                        <>
+                <div className="select-wrap">
+                    <label htmlFor="results-country-list" className="visually-hidden">Select Country results to show</label>
+                    <select id="results-country-list" value={""} onChange={handleCountryChange}>
+                        {selectedCountry ?
+                            <>
+                                <option
+                                    value={selectedCountry}> {`${selectedCountry} Results`}</option>
+                                <option
+                                    value={undefined} key={"All"}>All Countries
+                                </option>
+                            </> :
                             <option
-                                value={selectedCountry}> {`${selectedCountry} Results`}</option>
-                            <option
-                                value={undefined} key={"All"}>All Countries
-                            </option>
-                        </> :
-                        <option
-                            value={undefined}> {getCountryResultsTranslation(locale ?? "en")}</option>}
+                                value={undefined}> {getCountryResultsTranslation(locale ?? "en")}</option>}
 
 
-                    {Object.entries(countries).map(([countryCode, countryName]) => {
-                        if (selectedCountry !== countryCode) {
-                            return (<option key={countryCode} value={countryCode}>
-                                {countryName}
-                            </option>)
-                        }
-                    })}
-                </select>
+                        {Object.entries(countries).map(([countryCode, countryName]) => {
+                            if (selectedCountry !== countryCode) {
+                                return (<option key={countryCode} value={countryCode}>
+                                    {countryName}
+                                </option>)
+                            }
+                        })}
+                    </select>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="32" aria-hidden="true" fill="#fff"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
+                </div>
             </Col>
 
         </Row>
@@ -117,7 +109,6 @@ export const VoteResults = ({questionId, locale}: { questionId: string, locale?:
         <Row>
             <Col>
                 <div className={"chart-container"} data-hovered={isHovered.toString()}>
-                    
                     <Doughnut className={"chart"} data={chartData} options={chartOptions}
                               data-hovered={isHovered.toString()} onMouseEnter={() => setIsHovered(true)}
                               onMouseLeave={() => setIsHovered(false)}/>
