51a51fdcb7415546db88029c0442ad8845664d6f
James Bateson <jim.bateson@outlook.com>
2025-04-03 11:33:06 +0100

    style: arrow alignment for hub link panel cards

--- src/components/ContentCompoenent.tsx
+++ src/components/ContentCompoenent.tsx
@@ -105,7 +105,7 @@ const ContentComponent = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-hidden="true"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" fill="currentColor"></path></svg>
                     </a>
                     
-                    <a href="#" className="hub-card-link" style={{ ...styles.card }}>
+                    <a href="#" className="hub-card-link" style={ styles.card }>
                         <h3 style={styles.cardTitle}>Joining the Foundation</h3>
                         <p>A link to the page that allows those wishing to actively support the project to become members of the Foundation.</p>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-hidden="true"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" fill="currentColor"></path></svg>
@@ -129,7 +129,7 @@ const ContentComponent = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-hidden="true"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" fill="currentColor"></path></svg>
                     </a>
 
-                    <a href="#" className="hub-card-link" style={{ ...styles.card, display: 'flex', flexDirection: 'column' }}>
+                    <a href="#" className="hub-card-link" style={ styles.card }>
                         <h3 style={styles.cardTitle}>Donating</h3>
                         <p>A link to the ‘Why Donate’ crowdfunding platform to enable those withing to make modest contributions to functioning of the Foundation.</p>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-hidden="true"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" fill="currentColor"></path></svg>
