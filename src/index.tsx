6fdc817f4846810547c184f5a5c1de44b7535efe
James Bateson <jim.bateson@outlook.com>
2025-07-08 17:51:10 +0100

    chore: add password gate back in

--- src/index.tsx
+++ src/index.tsx
@@ -19,9 +19,9 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
     <React.StrictMode>
         <HelmetProvider> 
-            {/* <PasswordGate> */}
+            <PasswordGate>
                 <App></App>
-            {/* </PasswordGate> */}
+            </PasswordGate>
         </HelmetProvider>
     </React.StrictMode>
 );
