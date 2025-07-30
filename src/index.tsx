d772c5ee744ff1e3c99386d1d1efa2054a221af3
James Bateson <jim.bateson@outlook.com>
2025-07-30 15:49:06 +0100

    chore: remove password gate for testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from 'react-helmet-async';
 
-import "../src/fonts/rns-sans/RNSSanz-Normal.woff";
-import "../src/fonts/rns-sans/RNSSanz-Light.woff";
-import "../src/fonts/rns-sans/RNSSanz-Bold.woff";
-import "../src/fonts/rns-sans/RNSSanz-Normal.woff2";
-import "../src/fonts/rns-sans/RNSSanz-Light.woff2";
-import "../src/fonts/rns-sans/RNSSanz-Bold.woff2";
+import '../src/fonts/rns-sans/RNSSanz-Normal.woff';
+import '../src/fonts/rns-sans/RNSSanz-Light.woff';
+import '../src/fonts/rns-sans/RNSSanz-Bold.woff';
+import '../src/fonts/rns-sans/RNSSanz-Normal.woff2';
+import '../src/fonts/rns-sans/RNSSanz-Light.woff2';
+import '../src/fonts/rns-sans/RNSSanz-Bold.woff2';
 
+import './index.scss';
 
-import "./index.scss";
+import App from './App';
+import PasswordGate from './PasswordGate';
 
-import App from "./App";
-import PasswordGate from "./PasswordGate";
-
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
-        <HelmetProvider> 
-            <PasswordGate>
-                <App></App>
-            </PasswordGate>
-        </HelmetProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<HelmetProvider>
+			{/* <PasswordGate> */}
+			<App></App>
+			{/* </PasswordGate> */}
+		</HelmetProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
