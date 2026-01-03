285a3a08542158329a1032bf2203e6fe1d39c270
Chris Pepper <chrispepper1989@gmail.com>
2026-01-03 21:14:49 +0000

    fixing page button

--- src/repositories/utils/extraTranslations.ts
+++ src/repositories/utils/extraTranslations.ts
@@ -360,7 +360,7 @@ export function getNextTranslation(languageCode: string): string {
 }
 export const allTranslations: { [key: string]: { videoSummary: string, climateSummary: string } } = {
     "en": {
-        "videoSummary": "Follow the money",
+        "videoSummary": "The Idea",
         "climateSummary": "Climate summary"
     },
     "fr": {
