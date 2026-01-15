29f718b838f48a158e53a402a6016b6331f79a64
Chris Pepper <chrispepper1989@gmail.com>
2026-01-15 17:58:32 +0000

    tweak: changes ui

--- src/pages/VotingPageVariants/VotingPageMainJourney.tsx
+++ src/pages/VotingPageVariants/VotingPageMainJourney.tsx
@@ -144,7 +144,7 @@ export const StagedFlow = (props: TStagedFlowProps) => {
 									{props.landingHeading}
 								</h1>
 
-								<div style={{ fontSize: '1.2rem' }}>
+								<div style={{ fontSize: '0.95rem' }}>
 									<StructuredText data={props.openingText} />
 								</div>
 
--- src/repositories/utils/extraTranslations.ts
+++ src/repositories/utils/extraTranslations.ts
@@ -360,7 +360,7 @@ export function getNextTranslation(languageCode: string): string {
 }
 export const allTranslations: { [key: string]: { videoSummary: string, climateSummary: string } } = {
     "en": {
-        "videoSummary": "The Idea",
+        "videoSummary": "Introduction",
         "climateSummary": "Climate summary"
     },
     "fr": {
