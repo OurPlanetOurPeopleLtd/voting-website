a380600b91de32d2ba289c10ccb972100a939625
Christopher John Pepper <chrispepper1989@gmail.com>
2026-01-25 19:51:49 +0000

    Add lowercase variant of 'Anthony.S' to user list
    ...

--- src/PasswordGate.tsx
+++ src/PasswordGate.tsx
@@ -29,7 +29,8 @@ const PASSWORDS = [
 	'George.Monbiot',
 	'Carla.D',
 	'Zak.P',
-	'Anthony.S'
+	'Anthony.S',
+	'anthony.s'
 ];
 const STORAGE_KEY = 'site_unlocked';
 const MAGIC_GUID = '43722bdd-325b-46e4-8c95-1fb95a784b5f'; 
