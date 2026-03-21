10934ba152d9d5f9f2a20c8d3f1cd388c9a791b1
OurPlanetOurPeople <122091989+OurPlanetOurPeople@users.noreply.github.com>
2026-03-21 11:29:32 +0000

    Update PasswordGate.tsx

--- src/PasswordGate.tsx
+++ src/PasswordGate.tsx
@@ -42,7 +42,8 @@ const PASSWORDS = [
 	'SNP2027',
 	'sfsdfes',
 	'ClimateBreakthrough',
-	'ClimateLead'
+	'ClimateLead',
+	'EdMiliband'
 ];
 const STORAGE_KEY = 'site_unlocked';
 const MAGIC_GUID = '43722bdd-325b-46e4-8c95-1fb95a784b5f'; 
